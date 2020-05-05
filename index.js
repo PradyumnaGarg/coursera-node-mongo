01927a63ad0d0f2a571d6d234d602ea118232339
Veteran <pradumnagarg@hotmail.com>
2020-05-06 02:19:34 +0530

    Node MongoDB Part 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb');
 const assert = require('assert');
+const dbops = require('./operations');
 
 const url = 'mongodb://localhost:27017';
 const dbname = 'conFusion';
@@ -10,25 +11,29 @@ MongoClient.connect(url, (err, client) => {
     console.log('Connect correctly to server');
 
     const db = client.db(dbname);
-    const collection = db.collection("dishes");
-    collection.insertOne({"name":"Kachori", "description":"Tasty Tasty"}, 
-    (err, result) => {
-        assert.equal(err, null);
 
-        console.log("After Insert \n");
-        console.log(result.ops);
+    dbops.insertDocument(db, {name: "Pizza", description: "Delicious"}, 
+        "dishes", (result) => {
+            console.log("Inserted Document:\n", result.ops);
+            
+            dbops.findDocuments(db, "dishes", (docs) => {
+                console.log("Found Documents:\n", docs);
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err,null);
+                dbops.updateDocument(db, {name: "Pizza"}, {description: "Awesome"}, "dishes", (result) => {
+                    console.log("Updated Document:\n", result.result);
 
-            console.log("Found: \n");
-            console.log(docs);
-
-            db.dropCollection("dishes", (err, result) => {
-                assert.equal(err, null);
-
-                client.close();
+                    dbops.findDocuments(db, "dishes", (docs) => {
+                        console.log("Updated Doument: ", docs);
+                        
+                        db.dropCollection("dishes", (result) => {
+                            console.log("Dropped Collection: ", result);
+                            
+                            client.close();
+                        });
+                    });
+                });
             });
-        });
+
     });
+    
 });
